1c4ad4961d1fef79b86c2f73342c07f25e5d7fb5
Kelly Wenzel <aspenmarch@gmail.com>
2018-10-14 17:41:04 -0600

    added code and comments for "Loops" section

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Do-While-Statements.js
@@ -0,0 +1,10 @@
+// Write your code below
+let cupsOfSugarNeeded = 5;
+let cupsAdded = 0;
+
+do {
+ cupsAdded++
+} while (cupsAdded < cupsOfSugarNeeded);
+
+/*I learned that I could establish a do/while in order to have an action occur
+only while other arguments are in play.*/
